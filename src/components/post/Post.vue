186447e43c9e1d0c2b8e2c2cf1a085e581c154ee
i.sharapov <sharapov2701@gmail.com>
2024-12-02 16:25:19 +0300

    Добавить hover и active эффект для поста

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -12,13 +12,28 @@ const { editPost } = usePostStore()
 const textarea = useTemplateRef('textarea')
 
 const isEditing = ref<boolean>(false)
+const isFocused = ref<boolean>(false)
 const postName = ref<Post['name']>(props.post.name)
 
 const isSavingDisabled = computed<boolean>(() => props.post.name === postName.value)
 
+function focusPost() {
+  if (!isFocused.value) {
+    isFocused.value = true
+  }
+}
+
+function blurPost() {
+  nextTick(() => (isFocused.value = false))
+}
+
+function focusTextarea() {
+  textarea.value.focus()
+}
+
 function startEditing() {
   isEditing.value = true
-  textarea.value.focus()
+  focusTextarea()
 }
 
 function stopEditing() {
@@ -40,24 +55,43 @@ function save() {
 </script>
 
 <template>
-  <v-form @submit.prevent>
-    <v-textarea
-      ref="textarea"
-      v-model:model-value="postName"
-      rows="2"
-      auto-grow
-      variant="plain"
-      :label="post.id"
-      :readonly="!isEditing"
-    />
+  <v-hover>
+    <template #default="{ isHovering, props: hoverProps }">
+      <v-list-item
+        v-bind="hoverProps"
+        active-color="gray"
+        :active="isEditing || isFocused || isHovering"
+        @click="focusPost"
+        @blur="blurPost"
+      >
+        <v-form @submit.prevent>
+          <v-textarea
+            ref="textarea"
+            v-model:model-value="postName"
+            rows="2"
+            auto-grow
+            variant="plain"
+            :label="post.id"
+            :readonly="!isEditing"
+            @blur="blurPost"
+          />
 
-    <v-btn class="mr-2" @click="isEditing ? cancelEditing() : startEditing()">
-      <span v-if="isEditing"> Отмена </span>
-      <v-icon v-else icon="mdi-pencil" />
-    </v-btn>
+          <v-btn class="mr-2" @click.stop="isEditing ? cancelEditing() : startEditing()">
+            <span v-if="isEditing"> Отмена </span>
+            <v-icon v-else icon="mdi-pencil" />
+          </v-btn>
 
-    <v-btn v-if="isEditing" type="submit" color="blue" :disabled="isSavingDisabled" @click="save">
-      Сохранить
-    </v-btn>
-  </v-form>
+          <v-btn
+            v-if="isEditing"
+            type="submit"
+            color="blue"
+            :disabled="isSavingDisabled"
+            @click.stop="save"
+          >
+            Сохранить
+          </v-btn>
+        </v-form>
+      </v-list-item>
+    </template>
+  </v-hover>
 </template>
