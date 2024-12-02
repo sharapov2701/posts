81df154ca7b92f5119857771dd6f281db71d414f
i.sharapov <sharapov2701@gmail.com>
2024-12-02 06:55:44 +0300

    Настроить линтеры

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -12,9 +12,6 @@ import router from '../router'
 // Types
 import type { App } from 'vue'
 
-export function registerPlugins (app: App) {
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router).use(pinia)
 }
