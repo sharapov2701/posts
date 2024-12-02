844c45b14b072d997decae69f6d8ef595b9c149f
sharapov2701 <sharapov2701@users.noreply.github.com>
2024-12-02 20:06:05 +0000

    deploy: 5902dd824dab8431f18d1cd1b8cf3c88888b164c

--- assets/default-BHkKQWSx.js
+++
@@ -1,1 +0,0 @@
-import{d as o,c as s,w as e,V as n,r,o as c,a,b as _}from"./index-D6wzTd18.js";const f=o({__name:"default",setup(p){return(l,u)=>{const t=r("router-view");return c(),s(n,null,{default:e(()=>[a(_,{class:"py-6 h-screen"},{default:e(()=>[a(t)]),_:1})]),_:1})}}});export{f as default};
